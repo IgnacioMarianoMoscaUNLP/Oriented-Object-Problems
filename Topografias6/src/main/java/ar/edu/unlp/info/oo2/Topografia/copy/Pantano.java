c22da113e259440ceb9437c78cddc9b0d8ba8d90
ignacio mariano <ignaciomarianomosca@gmail.com>
2024-04-09 11:40:09 -0300

    agrega pantano topo

---
+++ Topografias6/src/main/java/ar/edu/unlp/info/oo2/Topografia/copy/Pantano.java
@@ -0,0 +1,37 @@
+package ar.edu.unlp.info.oo2.Topografia.copy;
+
+import java.util.List;
+
+public class Pantano extends Topografia{
+
+	@Override
+	public Integer proporcionAgua() {
+		// TODO Auto-generated method stub
+		return 7/10;
+	}
+
+	@Override
+	public Integer proporcionTierra() {
+		// TODO Auto-generated method stub
+		return 3/10;
+	}
+
+	@Override
+	public Boolean igualTierra(Integer d) {
+		// TODO Auto-generated method stub
+		return this.proporcionTierra()==d;
+	}
+
+	@Override
+	public Boolean igualAgua(Integer d) {
+		// TODO Auto-generated method stub
+		return this.proporcionAgua()==d;
+	}
+
+	@Override
+	public List getPartes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
