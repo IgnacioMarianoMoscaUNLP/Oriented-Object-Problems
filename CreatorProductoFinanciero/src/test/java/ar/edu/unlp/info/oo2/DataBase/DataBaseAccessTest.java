63ee57f2522f8ba9d9bbd7e2b9518c54be0183a6
ignacio mariano <ignaciomarianomosca@gmail.com>
2024-04-24 00:06:12 -0300

    comentario uml p17, ventaja: todo lo que conlleva el uso del facotry, no hace falta especificar una y otra vez el producto. desventaja: para nuevos productos hace falta el agregado de constructores/creadores concretos nuevos y cambiar la logica segun se haya indicado. habria que ver si mi solucion podria aplicar otro patron para el manejo del  tipo  de producto que tiene mi usuario o crea.

---
+++ CreatorProductoFinanciero/src/test/java/ar/edu/unlp/info/oo2/DataBase/DataBaseAccessTest.java
@@ -0,0 +1,28 @@
+package ar.edu.unlp.info.oo2.DataBase;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import java.util.ArrayList;
+
+public class DataBaseAccessTest {
+	
+	@BeforeEach
+	void setUp() throws Exception{
+		
+	}
+	
+	@Test
+	public void testLog() {
+			
+	}
+}
+
+	
+	
+
